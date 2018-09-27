18fa4d9b43c68d28e61d47604e51f085e18d1cde
johnli <1033096695@qq.com>
2018-09-27 19:54:41 +0800

    "18-09-27-v4"

---
+++ src/one_Foundation/Java_1_5/Ex11.java
@@ -0,0 +1,11 @@
+package one_Foundation.Java_1_5;
+
+public class Ex11 {
+    /*
+    类似于加权 quick-union 的做法，新增一个 size[] 数组以记录各个根节点的大小。
+
+    每次合并时先比较一下两棵树的大小，再进行合并。
+
+    这样会略微减少赋值语句的执行次数，提升性能。
+     */
+}
